88fc679fe652ef4577193ab0b4b9826be6dff08d
plummer <plummer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-09-10 12:23:48 +0000

    Include last updates from Bendix in old trunk directory
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/epi@62 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/MS.boxes.R
+++ pkg/R/MS.boxes.R
@@ -1,10 +1,10 @@
 tbox <-
 function( txt, x, y, w, h,
           font=2, txt.col="black",
-          lwd=2, border="black", col="transparent" )
+          lwd=2, border="black", col="transparent", ... )
 {
 rect( x-w/2, y-h/2, x+w/2, y+h/2, lwd=lwd, border=border, col=col )
-text( x, y, txt, font=font, col=txt.col )
+text( x, y, txt, font=font, col=txt.col, ... )
 invisible( c( x, y, w, h ) )
 }
 
@@ -34,15 +34,28 @@ arrows( x1 + (x2-x1)*(1-fr)/2,
         y2 - (y2-y1)*(1-fr)/2, angle=a, lwd=lwd, ... )
 }
 
-boxarr <-
-function( b1, b2, ... )
+std.vec <-
+function( a, b )
 {
-x1 <- b1[1]
-y1 <- b1[2]
+  l <- sqrt(a^2+b^2)
+  if( l==0 )
+  return( c(0,0) )
+  else
+  return( c(a/l,b/l) )
+}
+
+boxarr <-
+function( b1, b2, offset=FALSE, ... )
+{
+# If we want to offset the arrow a bit to the left
+if( offset ) d <- std.vec( b2[1]-b1[1], b2[2]-b1[2] )
+else d <- c(0,0)
+x1 <- b1[1] - d[2]
+y1 <- b1[2] + d[1]
 w1 <- b1[3]
 h1 <- b1[4]
-x2 <- b2[1]
-y2 <- b2[2]
+x2 <- b2[1] - d[2]
+y2 <- b2[2] + d[1]
 w2 <- b2[3]
 h2 <- b2[4]
 hx1 <- x1 + ifelse( (y2-y1)!=0, (x2-x1)*((h1/2)/abs(y2-y1)), sign(x2-x1)*w1/2 )
@@ -62,75 +75,165 @@ if( abs(vy2-y2) < h2/2 ) { bx2 <- vx2
                     else { bx2 <- hx2
                            by2 <- hy2 }
 fillarr( bx1, by1, bx2, by2, ... )
-invisible( list( x=(bx1+bx2)/2, y=(by1+by2)/2 ) )
+invisible( list( x=(bx1+bx2)/2,
+                 y=(by1+by2)/2,
+                 d=std.vec(bx2-bx1,by2-by1) ) )
 }
 
 boxes <- function (obj, ...) UseMethod("boxes")
 
 boxes.Lexis <-
-function( obj, file, mult=1.5, cex=1.5 )
+function( obj, file,
+                wmult = 1.5,
+                hmult = 1.5*wmult,
+                  cex = 1.5,
+               show   = inherits( obj, "Lexis" ),
+               show.Y = show,
+               show.D = show,
+              scale.Y = 1,
+             digits.Y = 1,
+                eq.wd = TRUE,
+                eq.ht = TRUE, ... )
 {
 if( inherits(obj,"Lexis") ) tm <- tmat( obj )
 else if( is.matrix(obj) & diff(dim(obj))==0 ) tm <- obj
 else stop( "First argument must be a Lexis object or a square matrix.\n" )
 
-# State names
-st.nam <- colnames( tm )
+# First get the number of transitions, then write it all to a file
+# and then source it to do the plot
+                      st.nam <- colnames( tm )
 if( is.null(st.nam) ) st.nam <- paste(1:ncol(tm))
-n.st <- length( st.nam )
+            pl.nam <- st.nam
+      n.st <- length( st.nam )
+xx <- yy <- numeric( n.st )
 
-# Here is the plot
+# Do we want to show person-years and events
+if( show & inherits( obj, "Lexis" ) )
+  {
+  TR <- summary(obj,simplify=FALSE,scale=scale.Y)$Transitions
+  Y <- TR[-nrow(TR),"Risk time:"]
+  D <- TR[-nrow(TR),-(nrow(TR):ncol(TR))]
+  }
+# No extra line with person-years when they are NA
+if( show.Y ) pl.nam <- gsub("\\\nNA", "",
+    paste( st.nam, formatC( Y, format="f", digits=digits.Y, big.mark="," ), sep="\n" ) )
+xx <- yy <- wd <- ht <- numeric( n.st )
+b <- list()
+
+# Her comes the plot
 par( mar=c(0,0,0,0), cex=cex )
 plot( NA,
       bty="n",
       xlim=0:1*100, ylim=0:1*100, xaxt="n", yaxt="n", xlab="", ylab="" )
-xx <- yy <- wd <- ht <- numeric(n.st)
-b <- list()
+# String height and width only meaningful after a plot has been called
+ht <- strheight( pl.nam ) * hmult
+wd <- strwidth(  pl.nam ) * wmult
+if( eq.ht ) ht <- rep( max(ht), length(ht) )
+if( eq.wd ) wd <- rep( max(wd), length(wd) )
+# Ask for positions of boxes
 for( i in 1:n.st )
  {
- txt <- st.nam[i]
- cat( "\nClick for level ", txt )
+ cat( "\nClick for level ", st.nam[i] )
  flush.console()
  pt <- locator(1)
  xx[i] <- pt$x
  yy[i] <- pt$y
- ht[i] <- strheight( txt ) * mult * 1.5
- wd[i] <- strwidth( txt ) * mult
- b[[i]] <- tbox( txt, xx[i], yy[i], wd[i], ht[i] )
+ b[[i]] <- tbox( pl.nam[i], xx[i], yy[i], wd[i], ht[i] )
  }
 cat( "\n" )
 
 for( i in 1:n.st ) for( j in 1:n.st )
-  if( !is.na(tm[i,j]) )
-    boxarr( b[[i]], b[[j]] )
-# If we want the code
+  {
+  if( !is.na(tm[i,j]) & i!=j )
+    {
+    arr <- boxarr( b[[i]], b[[j]], offset=!is.na(tm[j,i]) )
+    if( show.D )
+    text( arr$x+arr$d[2], arr$y-arr$d[1], paste( D[i,j] ),
+          adj=(as.numeric(c(arr$d[2]>0,arr$d[1]<0))-0.5)*1.5+0.5,
+          font=2, col="black" )
+    }
+  }
+
+#################################################################
+# If we want the code we just print the entire function code,
+# where we have replaced the query for the coordinates of the
+# boxes with the values we obtained from locator()
 if( !missing(file) )
 {
-xx <- round(xx,1)
-yy <- round(yy,1)
-wd <- round(wd,1)
-ht <- round(ht,1)
-obj.name <- substitute( deparse( obj ) )
+xx <- round(xx)
+yy <- round(yy)
+
+# Here we start writing the file, beginning with setting the parameters
 cat( '
-st.nam <- colnames( tm )
+obj <- ', deparse( substitute( obj ) ), '\n
+if( inherits(obj,"Lexis") ) tm <- tmat.Lexis( obj ) else tm <- obj
+cex <-', cex, ' # How should text and numbers be scaled
+wmult <-', wmult, ' # Extra box-width relative to string width
+hmult <-', hmult, ' # Extra box-height relative to string height
+eq.wd <-', eq.wd, ' # All boxes the same width
+eq.ht <-', eq.ht, ' # All boxes the same height
+show.D <-', show.D, ' # Show number of events on arrows
+show.Y <-', show.Y, ' # Show number of person-years in boxes
+scale.Y <-', scale.Y, ' # How should person-years be scaled
+
+# First get the number of transitions, then write it all to a file
+# and then source it to do the plot
+                      st.nam <- colnames( tm )
 if( is.null(st.nam) ) st.nam <- paste(1:ncol(tm))
-n.st <- length( st.nam )
-b <- list(NULL)
-par( mar=c(0,0,0,0), cex=1.5 )
+            pl.nam <- st.nam
+      n.st <- length( st.nam )
+xx <- yy <- numeric( n.st )
+
+# Do we want to show person-years and events ?
+if( inherits( obj, "Lexis" ) )
+  {
+#################################################################
+### Adjust the scaling of the person-Years
+  TR <- summary(obj,simplify=FALSE,scale=scale.Y)$Transitions
+  Y <- TR[-nrow(TR),"Risk time:"]
+  D <- TR[-nrow(TR),-(nrow(TR):ncol(TR))]
+  }
+
+# No extra line with person-years when they are NA
+if( show.Y ) pl.nam <- gsub("\\\nNA", "",
+#################################################################
+### Adjust the printing of the person-Years
+    paste( st.nam, formatC( Y, format="f", digits=',digits.Y,', big.mark=","), sep="\n" ) )
+xx <- yy <- wd <- ht <- numeric( n.st )
+
+#################################################################
+### Position of the boxes:
+xx <- c(', paste( xx, collapse=", " ),')
+yy <- c(', paste( yy, collapse=", " ),')
+b <- list()
+
+# Here comes the plot
+par( mar=c(0,0,0,0), cex=cex )
 plot( NA,
       bty="n",
-      xlim=0:1*100, ylim=0:1*100, xaxt="n", yaxt="n", xlab="", ylab="" )',
-      file = file )
+      xlim=0:1*100, ylim=0:1*100, xaxt="n", yaxt="n", xlab="", ylab="" )
+# String height and width only meaningful after a plot has been called
+ht <- strheight( pl.nam ) * hmult
+wd <- strwidth(  pl.nam ) * wmult
+#################################################################
+### Should all boxes be of same height / width:
+if( eq.ht ) ht <- rep( max(ht), length(ht) )
+if( eq.wd ) wd <- rep( max(wd), length(wd) )
+# Plot the boxes
 for( i in 1:n.st )
-cat( 'b[[',paste(i),']] <-\ntbox("',st.nam[i],'",',
-     paste(c(xx[i],yy[i],wd[i],ht[i]),collapse=","),",",
-'\n         font=2, txt.col="black",',
-'\n         lwd=2, border="black", col="transparent" )\n',
-     sep="", file=file, append=TRUE )
+ b[[i]] <- tbox( pl.nam[i], xx[i], yy[i], wd[i], ht[i] )
+# Plot the arrows
 for( i in 1:n.st ) for( j in 1:n.st )
+  {
   if( !is.na(tm[i,j]) )
-cat( 'boxarr( b[[',paste(i),']], b[[',paste(j),']], fr=0.8,
-             angle=15, lwd=2, length=par("pin")[1]/30 )\n',
-      sep="", file=file, append=T )
+    {
+    arr <- boxarr( b[[i]], b[[j]], offset=!is.na(tm[j,i]) )
+########################################################################
+###  Use the adj=  parameter to position the no. transitions rel. to arrow
+    if( show.D )
+    text( arr, paste( D[i,j] ), adj=c(1.5,1.5), font=2, col="black" )
+    }
+  }
+     ', file=file )
 }
 }
