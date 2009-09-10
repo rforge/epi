4f20b0d5a905a039279b8c83943e04dfef310afa
plummer <plummer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-09-10 12:24:26 +0000

    Delete old R and man directories
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/epi@63 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- Epi/trunk/R/as.Date.cal.yr.R
+++
@@ -1,5 +0,0 @@
-as.Date.cal.yr <-
-function( x, ... )
-{
-structure( round( ( x - 1970 ) * 365.25 ), class="Date" )
-}
--- Epi/trunk/R/print.Icens.r
+++
@@ -1,6 +0,0 @@
-print.Icens <-
-function( x, digits=4, scale=1, ... )
-{
-emat <- summary.Icens( x, scale=scale )
-print( round( emat, digits ) )
-}
