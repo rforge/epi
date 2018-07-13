8b9cc4ec854a8bae924e9a5d78992b6e7caccde5
bxc <bxc@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-07-13 11:01:17 +0000

    Major update of gen.exp, addition of in.span, and glm.Lexis etc. Documentation edits
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/epi@221 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/gen.exp.R
+++ pkg/R/gen.exp.R
@@ -3,11 +3,14 @@
 # dpt: dose per time
 # amt: amount
 # dur: duration
+# dof: date of follow-up
 
+# A function to compute cumulative doses etc. at the purchase dates
+# *and* derive intervals of drug coverage resp no coverage from the
+# purchase date, amounts and dosage per time 
 use.amt.dpt <-
-function( purchase,
+function( purchase, # data frame with dop, amt and dpt
           push.max = Inf,
-            breaks,
               lags = NULL,
            lag.dec = 1 )
 {
@@ -37,7 +40,7 @@ lapply( split( purchase, purchase$id ),
         # Compute the end of the intervals
         exp.end   <- exp.start + drug.dur
         # Intervals in the middle not covered by the drug exposures - note
-        # also that we make a record for the last follow-date
+        # also that we make a record for the last follow-up date
         followed.by.gap <- c( exp.start[-1]-exp.end[-length(exp.end)] > 0, TRUE )
         # To facilitate
         dfR <- rbind( data.frame( id = set$id[1],
@@ -47,9 +50,12 @@ lapply( split( purchase, purchase$id ),
                                  dof = exp.end[followed.by.gap],
                                  dpt = 0 ) )
         dfR <- dfR[order(dfR$dof),]
-        # We now compute the cumulative dose at the end of the interval using
-        # interval length and dpt:
-        dfR$cum.amt <- with( dfR, cumsum( c(0, diff(dof)*dpt[-length(dpt)]) ) )
+        # Logical indicator of intervals off drug
+        dfR$off <- (dfR$dpt==0)
+        # Finally compute the cumulative dose and time on drug at the end of the interval
+        # using interval length and dpt:
+        dfR$cum.amt <- with( dfR, cumsum( c(0, diff(dof)* dpt[-length(dpt)]) ) )
+        dfR$cum.tim <- with( dfR, cumsum( c(0, diff(dof)*!off[-length(dpt)]) ) )
         return( dfR )
         } ) )
 }
@@ -57,7 +63,6 @@ lapply( split( purchase, purchase$id ),
 use.only.amt <-
 function( purchase,
           pred.win = Inf,
-            breaks,
               lags = NULL,
            lag.dec = 1 )
 {
@@ -77,21 +82,28 @@ lapply( split( purchase, purchase$id ),
         # but at least the last two purchase dates,
         wp <- ( set$dop > pmin( max(set$dop)-pred.win,
                                sort(set$dop,decreasing=TRUE)[2] ) )
-        # Cumulative amount consumed at each dop
+        # Cumulative amount consumed at each dop since first
         cum.amt <- cumsum(c(0,set$amt))
-        # Average slope to use to project the duration last purchase
+        # Average slope to use to project the duration of last purchase
         avg.slp <- diff(range(cum.amt[c(wp,FALSE)]))/
                    diff(range(set$dop[wp]))
         # Purchase dates and the date of last consumption
         dof <- c( set$dop, set$dop[np]+set$amt[np]/avg.slp )
+        # Cumulative time since first
+        cum.tim <- dof - set$dop[1]
+        # The only time without drug is after last dispense expired
+        off <- rep( c(FALSE,TRUE), c(length(dof)-1,1) )
         return( data.frame( id = set$id[1],
                            dof = dof,
-                       cum.amt = cum.amt ) )
+                       cum.amt = cum.amt,
+                       cum.tim = cum.tim,
+                           off = off ) ) # will be used in the subsequent code
         } ) )
 }
 
+# The revised version called gen.ex
 gen.exp <-
-function( purchase, id="id", dop="dop", amt="amt", dpt="dpt",
+function( purchase,  id="id" , dop="dop", amt="amt", dpt="dpt",
                 fu, doe="doe", dox="dox",
             breaks,
            use.dpt = ( dpt %in% names(purchase) ),
@@ -100,64 +112,109 @@ function( purchase, id="id", dop="dop", amt="amt", dpt="dpt",
           pred.win = Inf,
            lag.dec = 1 )
 {
-# Make sure that the data fames have the right column names
+# to aviot a NOTE
+dof <- NULL
+# Make sure that the data frames have the right column names
 wh <- match( c(id,dop,amt), names(purchase) )
 if( any( is.na(wh) ) ) stop("Wrong column names for the purchase data frame")
 names( purchase )[wh] <- c("id","dop","amt")
+    
+# Allow dpt to be entered as numerical scalar common for all records
+if( use.dpt )
+  if( is.numeric(dpt) )
+    {
+    if( length(dpt) > 1 ) stop("If dpt is numeric it must have length 1\n",
+        "otherwise it must be the name of the dpt variable in the dataset")
+    purchase$dpt <- dpt
+    }
+  else names( purchase )[match(dpt,names(purchase))] <- "dpt"
+
 wh <- match( c(id,doe,dox), names(fu) )
 if( any( is.na(wh) ) ) stop("Wrong column names for the follow-up data frame")
 names( fu )[wh] <- c("id","doe","dox")
+    
+if( use.dpt ) { tmp.dfr <- use.amt.dpt( purchase,
+                                        lags = lags,
+                                    push.max = push.max,
+                                     lag.dec = lag.dec )
+      } else { tmp.dfr <- use.only.amt( purchase,
+                                        lags = lags,
+                                    pred.win = pred.win,
+                                     lag.dec = lag.dec ) }
 
-if( use.dpt )
-  {
-  # This is to allow dpt to be entered as numerical scalar common for all
-  if( is.numeric(dpt) )
-    {
-    if( length(dpt) > 1 ) stop( "If dpt is numeric it must have length 1" )
-    purchase$dpt <- dpt
-    }
-  else
-  names( purchase )[match(c(dpt),names(purchase))] <- "dpt"
-  tmp.dfr <- use.amt.dpt( purchase,
-                              lags = lags,
-                          push.max = push.max,
-                           lag.dec = lag.dec )
-  }
-else
-  tmp.dfr <- use.only.amt( purchase,
-                               lags = lags,
-                           pred.win = pred.win,
-                            lag.dec = lag.dec )
+# Having done what needs to be done about the purchase records we turn
+# to the follow-up records, but we need to accommodate the fact that
+# there might be more fu records per person:
 
+# Generate record no within id in fu (ordering in fu is immaterial)
+fu$no <- ave( fu$id, fu$id, FUN=function(x) 1:length(x) )
+print( fu )
+# Set up the object to collect the resulting follow-up
+res <- NULL
 
-# Merge in the follow-up period for the persons
-tmp.dfr <- merge( tmp.dfr, fu, all=T )
-
-# Interpolate to find the cumulative doses at the dates in breaks
+# loop through subsets of fu with different ids
+for( ni in 1:max(fu$no) )
+   {    
+# Merge the follow-up period for the persons to the correponding
+# exposure records (hence the all.y=T)
+tm.dfr <- merge( tmp.dfr, fu[fu$no==ni,], by="id", all.y=TRUE )
+       
+# Interpolate to find the cumulative doses at the dates in the vector breaks
+res.dfr <-
 do.call( "rbind",
-lapply( split( tmp.dfr, tmp.dfr$id ),
+lapply( split( tm.dfr, tm.dfr$id ),
         function(set)
         {
         # All values of these are identical within each set (=person)
         doe <- set$doe[1]
         dox <- set$dox[1]
-        # The first and last date of exposure according to the assumption
+        # The first and last date of drug exposure according to the assumption
         doi <- min(set$dof)
         doc <- max(set$dof)
-        # Get the breakpoints and the entry end exit dates
+        # Breakpoints and the entry end exit dates are date we care for
         breaks <- sort( unique( c(breaks,doe,dox) ) )
+        # only the breaks inside the follow-up interval
         xval   <- breaks[breaks>=doe & breaks<=dox]
-        dfr    <- data.frame( id = set$id[1],
-                             dof = xval )
-        dfr$tfi  <- pmax(0,xval-doi)
-        dfr$tfc  <- pmax(0,xval-doc)
+        # Merge a) the purchase dates (set$dof) and
+        #    indicator being off drug (set$off) and
+        # the date a person goes off drug, doff (set to NA for remaining records)
+        # with b) the break dates (which is where we want things computed)
+        # Note we merge on the variable dof.    
+        dfr <-  merge( data.frame( set[,c("id","dof","off")],
+                                   doff = ifelse(set$off,set$dof,NA) ),
+                       data.frame( id = set$id[1],
+                                  dof = xval ), 
+                       all=TRUE )
+        # carry the off drug indicator forward to all break dates
+        dfr$off <- na.locf( dfr$off, na.rm=FALSE )
+        # possible NAs are before any drug purchase
+        dfr$off <- ifelse( is.na(dfr$off), FALSE, dfr$off )
+        # carry date of going off drug forward, but only for the off period
+        dfr$doff <- na.locf( dfr$doff, na.rm=FALSE ) * ifelse(dfr$off,1,NA)
+        # time from cessation can now be computed    
+        dfr$tfc <- dfr$dof - dfr$doff
+        # time from initiation of drug
+        dfr$tfi  <- pmax( 0, dfr$dof-doi )
+        # restrict to the desired timepoints
+        dfr <- subset( dfr, dof %in% xval )
+        # linear interpolation of the cumulative dose and time from the
+        # purchase data (set)
         dfr$cdos <- approx( set$dof, set$cum.amt, xout=xval, rule=2 )$y
+        dfr$ctim <- approx( set$dof, set$cum.tim, xout=xval, rule=2 )$y
+        # the same for the desired lags    
         for( lg in lags )
            dfr[,paste( "ldos",
                        formatC(lg,format="f",digits=lag.dec),
                        sep="." )] <-
            approx( set$dof, set$cum.amt, xout=xval-lg, rule=2 )$y
-        dfr
-        } ) )
-}
-
+        dfr$doff <- na.locf( dfr$doff, na.rm=FALSE )
+        dfr$tfc[is.na(dfr$tfc)] <- 0
+        dfr$dur <- c( diff(dfr$dof), NA )
+        return( dfr[-nrow(dfr),] )
+        } ) ) # end of do.call
+   res <- rbind( res, res.dfr ) # append to result from other fu$no 
+   } # end of for( ni in 1:max(fu$no) )
+var.order <- c("id","dof","dur","off","doff","tfc","tfi","ctim","cdos",
+               names(res)[grep("ldos",names(res))] )    
+res[order(res$id,res$dof),var.order]
+} # end of gen.ex
