9df1fedb2fd1b56719078f285eb730f80726cab2
bxc <bxc@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-08-19 12:37:31 +0000

    Fixes of gen.exp, glm and coxph for Lexis
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/epi@223 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/gen.exp.R
+++ pkg/R/gen.exp.R
@@ -5,21 +5,31 @@
 # dur: duration
 # dof: date of follow-up
 
+######################################################################
 # A function to compute cumulative doses etc. at the purchase dates
 # *and* derive intervals of drug coverage resp no coverage from the
-# purchase date, amounts and dosage per time 
+# purchase date, amounts and dosage per time
 use.amt.dpt <-
 function( purchase, # data frame with dop, amt and dpt
-          push.max = Inf,
-              lags = NULL,
-           lag.dec = 1 )
+          push.max,
+          rm.dose )
 {
 do.call( "rbind",
 lapply( split( purchase, purchase$id ),
         function(set)
         {
         np <- nrow(set)
-        if( np==1 ) return( NULL )
+        if( np==1 ) # return data frame of 2 rows
+          {
+          dfR <- data.frame( id = set$id,
+                            dof = set$dop + c(0,set$amt/set$dpt),
+                            amt = c(set$amt,0),
+                            dpt = c(set$dpt,0),
+                            off = c(FALSE,TRUE),
+                        cum.amt = c(0,set$amt),
+                        cum.tim = c(0,set$amt/set$dpt) )
+          return( dfR )
+          }
         set <- set[order(set$dop),]
         # Compute length of exposure periods
         drug.dur  <- set$amt / set$dpt
@@ -29,9 +39,9 @@ lapply( split( purchase, purchase$id ),
         # the dop
         exp.start <- new.start + cummax( pmax(set$dop-new.start,0) )
         # Compute the pushes
-        push.one <- exp.start - set$dop
+        push.one  <- exp.start - set$dop
         # Revise them to the maximally acceptable
-        push.adj <- pmin( push.one, push.max )
+        push.adj  <- pmin( push.one, push.max )
         # Revise the starting dates of exposure
         exp.start <- exp.start - push.one + push.adj
         # Revise the durations to be at most equal to differences between the
@@ -39,37 +49,40 @@ lapply( split( purchase, purchase$id ),
         drug.dur  <- pmin( drug.dur, c(diff(exp.start),Inf) )
         # Compute the end of the intervals
         exp.end   <- exp.start + drug.dur
-        # Intervals in the middle not covered by the drug exposures - note
-        # also that we make a record for the last follow-up date
-        followed.by.gap <- c( exp.start[-1]-exp.end[-length(exp.end)] > 0, TRUE )
-        # To facilitate
+        # Find out which exposure intervals that are followed by a gap:
+        followed.by.gap <- c( exp.start[-1]-exp.end[-np] > 0, TRUE )
+        # To facilitate further calculations we add records for the gaps
+        # and also a record for the date of drug expiration
         dfR <- rbind( data.frame( id = set$id[1],
                                  dof = exp.start,
+                                 amt = set$amt,
                                  dpt = set$dpt ),
                       data.frame( id = set$id[1],
                                  dof = exp.end[followed.by.gap],
+                                 amt = 0,
                                  dpt = 0 ) )
         dfR <- dfR[order(dfR$dof),]
         # Logical indicator of intervals off drug
-        dfR$off <- (dfR$dpt==0)
-        # Finally compute the cumulative dose and time on drug at the end of the interval
-        # using interval length and dpt:
+        dfR$off <- (dfR$dpt==0) | (dfR$dof < min(set$dop))
+        # Finally compute the cumulative dose and time on drug at the end of
+        # the interval using interval length and dpt:
+        if( rm.dose ) {
         dfR$cum.amt <- with( dfR, cumsum( c(0, diff(dof)* dpt[-length(dpt)]) ) )
+        } else dfR$cum.amt <- cumsum( c( 0, dfR$amt[-nrow(dfR)] ) )
         dfR$cum.tim <- with( dfR, cumsum( c(0, diff(dof)*!off[-length(dpt)]) ) )
         return( dfR )
         } ) )
 }
 
-use.only.amt <-
-function( purchase,
-          pred.win = Inf,
-              lags = NULL,
-           lag.dec = 1 )
-{
+######################################################################
 # Compute the cumulative dose at all purcase dates and at the last
 # (unknown) future expiry date, computed based on previous
 # consumption.  The resulting data frame has one more line per person
 # than no. of purchases.
+use.only.amt <-
+function( purchase,
+          pred.win )
+{
 do.call( "rbind",
 lapply( split( purchase, purchase$id ),
         function(set)
@@ -101,17 +114,78 @@ lapply( split( purchase, purchase$id ),
         } ) )
 }
 
-# The revised version called gen.ex
+######################################################################
+# Function to interpolate the drug exposures to the dates of FU.
+dex.int <-
+function( set, breaks, lags, lag.dec )
+{
+  dof <- NULL
+  # If only one record return null
+  if( nrow(set) < 2 ) return( NULL )
+  # All values of these are identical within each set (=person)
+  doe <- set$doe[1]
+  dox <- set$dox[1]
+  # The first date of drug exposure according to the assumption
+  doi <- min(set$dof)
+  # Breakpoints and the entry end exit dates are the dates we care for
+  # - but only within breaks
+  doin <- max( min(breaks), doe )
+  doex <- min( max(breaks), dox )
+  breaks <- sort( unique( c(breaks,doin,doex) ) )
+  # only the breaks inside the follow-up interval
+  xval <- breaks[breaks>=doin & breaks<=doex]
+  if( length(xval) == 0 ) return( NULL )
+  # Merge a) the purchase dates (set$dof) and
+  #    indicator being off drug (set$off) and
+  # the date a person goes off drug, doff (set to NA for remaining records)
+  # with b) the break dates (which is where we want things computed)
+  # Note we merge on the variable dof.
+  dfr <-  merge( data.frame( set[,c("id","dof","off")],
+                             doff = ifelse(set$off,set$dof,NA) ),
+                 data.frame( id = set$id[1],
+                            dof = xval ),
+                 all=TRUE )
+  # carry the off drug indicator forward to all break dates
+  dfr$off <- zoo::na.locf( dfr$off, na.rm=FALSE )
+  # possible NAs are before any drug purchase hence off-drug
+  dfr$off <- ifelse( is.na(dfr$off), TRUE, dfr$off )
+  # carry date of going off drug forward, but only for the off period
+  dfr$doff <- zoo::na.locf( dfr$doff, na.rm=FALSE ) * ifelse(dfr$off,1,NA)
+  # time from cessation can now be computed
+  dfr$tfc <- dfr$dof - dfr$doff
+  # time from initiation of drug
+  dfr$tfi  <- pmax( 0, dfr$dof-doi )
+  # restrict to the desired timepoints
+  dfr <- subset( dfr, dof %in% xval )
+  # linear interpolation of the cumulative dose and time from the
+  # purchase data (set)
+  dfr$cdos <- approx( set$dof, set$cum.amt, xout=xval, rule=2 )$y
+  dfr$ctim <- approx( set$dof, set$cum.tim, xout=xval, rule=2 )$y
+  # the same for the desired lags
+  for( lg in lags )
+     dfr[,paste( "lag.pre",
+                 formatC(lg,format="f",digits=lag.dec),
+                 sep="" )] <-
+     approx( set$dof, set$cum.amt, xout=xval-lg, rule=2 )$y
+  dfr$doff <- zoo::na.locf( dfr$doff, na.rm=FALSE )
+  dfr$tfc[is.na(dfr$tfc)] <- 0
+  dfr$dur <- c( diff(dfr$dof), NA )
+  return( dfr[-nrow(dfr),] )
+  }
+
+######################################################################
+# The function that ties it all together
 gen.exp <-
 function( purchase,  id="id" , dop="dop", amt="amt", dpt="dpt",
                 fu, doe="doe", dox="dox",
             breaks,
            use.dpt = ( dpt %in% names(purchase) ),
-              lags = NULL,
           push.max = Inf,
-          pred.win = Inf,
+           rm.dose = FALSE,
+              lags = NULL,
            lag.dec = 1,
-            prefix = "lag." )
+           lag.pre = "lag.",
+          pred.win = Inf )
 {
 # to aviod a NOTE from R CMD check
 dof <- NULL
@@ -119,7 +193,7 @@ dof <- NULL
 wh <- match( c(id,dop,amt), names(purchase) )
 if( any( is.na(wh) ) ) stop("Wrong column names for the purchase data frame")
 names( purchase )[wh] <- c("id","dop","amt")
-    
+
 # Allow dpt to be entered as numerical scalar common for all records
 if( use.dpt )
   if( is.numeric(dpt) )
@@ -133,15 +207,12 @@ if( use.dpt )
 wh <- match( c(id,doe,dox), names(fu) )
 if( any( is.na(wh) ) ) stop("Wrong column names for the follow-up data frame")
 names( fu )[wh] <- c("id","doe","dox")
-    
+
 if( use.dpt ) { tmp.dfr <- use.amt.dpt( purchase,
-                                        lags = lags,
-                                    push.max = push.max,
-                                     lag.dec = lag.dec )
-      } else { tmp.dfr <- use.only.amt( purchase,
-                                        lags = lags,
-                                    pred.win = pred.win,
-                                     lag.dec = lag.dec ) }
+                                        push.max = push.max,
+                                        rm.dose = rm.dose )
+       } else { tmp.dfr <- use.only.amt( purchase,
+                                         pred.win = pred.win ) }
 
 # Having done what needs to be done about the purchase records we turn
 # to the follow-up records, but we first need to accommodate the fact
@@ -154,69 +225,24 @@ res <- NULL
 
 # loop through subsets of fu with different ids
 for( ni in 1:max(fu$no) )
-   {    
+   {
 # Merge the follow-up period for the persons to the correponding
 # exposure records (hence the all.y=T)
 tm.dfr <- merge( tmp.dfr, fu[fu$no==ni,], by="id", all.y=TRUE )
-       
+
 # Interpolate to find the cumulative doses at the dates in the vector breaks
 res.dfr <-
 do.call( "rbind",
-lapply( split( tm.dfr, tm.dfr$id ),
-        function(set)
-        {
-        # All values of these are identical within each set (=person)
-        doe <- set$doe[1]
-        dox <- set$dox[1]
-        # The first and last date of drug exposure according to the assumption
-        doi <- min(set$dof)
-        doc <- max(set$dof)
-        # Breakpoints and the entry end exit dates are the dates we care for
-        breaks <- sort( unique( c(breaks,doe,dox) ) )
-        # only the breaks inside the follow-up interval
-        xval   <- breaks[breaks>=doe & breaks<=dox]
-        # Merge a) the purchase dates (set$dof) and
-        #    indicator being off drug (set$off) and
-        # the date a person goes off drug, doff (set to NA for remaining records)
-        # with b) the break dates (which is where we want things computed)
-        # Note we merge on the variable dof.    
-        dfr <-  merge( data.frame( set[,c("id","dof","off")],
-                                   doff = ifelse(set$off,set$dof,NA) ),
-                       data.frame( id = set$id[1],
-                                  dof = xval ), 
-                       all=TRUE )
-        # carry the off drug indicator forward to all break dates
-        dfr$off <- na.locf( dfr$off, na.rm=FALSE )
-        # possible NAs are before any drug purchase
-        dfr$off <- ifelse( is.na(dfr$off), FALSE, dfr$off )
-        # carry date of going off drug forward, but only for the off period
-        dfr$doff <- na.locf( dfr$doff, na.rm=FALSE ) * ifelse(dfr$off,1,NA)
-        # time from cessation can now be computed    
-        dfr$tfc <- dfr$dof - dfr$doff
-        # time from initiation of drug
-        dfr$tfi  <- pmax( 0, dfr$dof-doi )
-        # restrict to the desired timepoints
-        dfr <- subset( dfr, dof %in% xval )
-        # linear interpolation of the cumulative dose and time from the
-        # purchase data (set)
-        dfr$cdos <- approx( set$dof, set$cum.amt, xout=xval, rule=2 )$y
-        dfr$ctim <- approx( set$dof, set$cum.tim, xout=xval, rule=2 )$y
-        # the same for the desired lags    
-        for( lg in lags )
-           dfr[,paste( "lag.prefix",
-                       formatC(lg,format="f",digits=lag.dec),
-                       sep="" )] <-
-           approx( set$dof, set$cum.amt, xout=xval-lg, rule=2 )$y
-        dfr$doff <- na.locf( dfr$doff, na.rm=FALSE )
-        dfr$tfc[is.na(dfr$tfc)] <- 0
-        dfr$dur <- c( diff(dfr$dof), NA )
-        return( dfr[-nrow(dfr),] )
-        } ) ) # end of do.call
-   res <- rbind( res, res.dfr ) # append to result from other fu$no 
+         lapply( split( tm.dfr, tm.dfr$id ),
+                 dex.int,
+                 breaks, lags, lag.dec ) ) # end of do.call
+res <- rbind( res, res.dfr ) # append to result from other fu$no
    } # end of for( ni in 1:max(fu$no) )
+    
 var.order <- c("id","dof","dur","off","doff","tfc","tfi","ctim","cdos",
-               names(res)[grep("lag.prefix",names(res))] )    
+               names(res)[grep("lag.prefix",names(res))] )
 res <- res[order(res$id,res$dof),var.order]
-names(res) <- gsub( "lag.prefix", prefix, names(res) )
+names(res) <- gsub( "lag.prefix", lag.pre, names(res) )
 res
-} # end of gen.ex
+}
+# end of gen.ex
--- pkg/R/mod.Lexis.R
+++ pkg/R/mod.Lexis.R
@@ -1,12 +1,13 @@
-
-mod.Lexis <- 
-function( Lx, resp, formula, xpos, link="log", scale, ..., model )
+modLexis <- 
+function( Lx, resp, formula, xpos, link="log", scale, verbose=TRUE, ..., model )
 {
 # A common wrapper for glm and gam modeling of Lexis FU
   
 # is this a Lexis object ?
 if( !inherits(Lx,"Lexis") ) stop( "The first argument must be a Lexis object.\n")
 nameLx <- deparse(substitute(Lx))
+# Beginning of a new feature with a countmultiplier of the transitions considered 
+if( !("lex.N" %in% names(Lx)) ) Lx$lex.N <- 1
     
 # check that events are actual levels of lex.Xst 
 if( is.numeric(resp) ) resp <- levels( Lx$lex.Xst )[resp]
@@ -15,8 +16,8 @@ if( any(is.na(wh)) ) stop("'resp' must be a subset of: '",
                      paste(levels(Lx$lex.Xst), collapse="','", sep=""), "'\n" )  
 
 # is xpos supplied?
-if( missing(xpos) )
-  { xpos <- levels( factor(Lx$lex.Cst) )
+if( missing(xpos) ) {
+  xpos <- levels( factor(Lx$lex.Cst) )
   } else {
 # check that xpos are actual levels of lex.Cst 
 if( is.numeric(xpos) ) xpos <- levels( Lx$lex.Cst )[xpos]
@@ -29,8 +30,8 @@ Lx <- Lx[Lx$lex.Cst %in% xpos,]
 # construct the model formula - note that we want the possibility of
 # transitions to transient states, hence the lex.Xst != lex.Cst
 if( length(formula) != 2 ) stop( "formula must be a one-sided formula")
-form <- cbind( Lx$lex.Xst %in% resp &
-               Lx$lex.Xst != Lx$lex.Cst,
+form <- cbind( (Lx$lex.Xst %in% resp &
+                Lx$lex.Xst != Lx$lex.Cst)*Lx$lex.N,
                Lx$lex.dur ) ~ 1
 form[3] <- formula[2]
 xpos <- levels( factor(Lx$lex.Cst) ) # only levels present in lex.Cst
@@ -38,36 +39,32 @@ xpos <- levels( factor(Lx$lex.Cst) ) # only levels present in lex.Cst
 # Scaling
 Lx$lex.dur <- Lx$lex.dur/scale
     
-# Tell what we intend to and then do it    
+# Tell what we intend to and then do it
+if( verbose ){
 cat( deparse(substitute(model)),
      " Poisson analysis of Lexis object ", nameLx, " with ", link, " link",
      ":\n Transition rates from '", paste( xpos, collapse="','"), 
                           "' to '", paste( resp, collapse="','"), "'",
      if( scale!=1 ) paste(" scaled by", scale ), "\n", sep="" )
-
-#if( link=="identity" ) {
-#   mll <- model( form, family = poisreg, data = Lx )
-#   mod <- model( form, family = poisreg(link="identity"),
-#                      mustart = fitted(mll),
-#                         data = Lx, ... )
-#                       } else {
+             }
+    
+# Fit the model
 mod <- model( form, family = poisreg(link=link), data = Lx, ... )
-# }
 
 # An explanatory attribute
-attr( mod, "Lexis" ) <- list( Exposure=xpos, Events=resp )     
+attr( mod, "Lexis" ) <- list( Exposure=xpos, Events=resp, scale=scale )     
 mod
 }
 
 # Here are the actual functions of interest
 
 glm.Lexis <- 
- function( Lx, resp, formula, xpos, link="log", scale=1    , ... ) {
-mod.Lexis( Lx, resp, formula, xpos, link=link , scale=scale, ..., model=stats::glm ) }
+function( Lx, resp, formula, xpos, link="log", scale=1    , verbose=TRUE   , ... ) {
+modLexis( Lx, resp, formula, xpos, link=link , scale=scale, verbose=verbose, ..., model=stats::glm ) }
 
 gam.Lexis <- 
- function( Lx, resp, formula, xpos, link="log", scale=1    , ... ) {
-mod.Lexis( Lx, resp, formula, xpos, link=link , scale=scale, ..., model= mgcv::gam ) }
+function( Lx, resp, formula, xpos, link="log", scale=1    , verbose=TRUE   , ... ) {
+modLexis( Lx, resp, formula, xpos, link=link , scale=scale, verbose=verbose, ..., model= mgcv::gam ) }
 
 # and here the coxph counterpart:
 
@@ -76,6 +73,7 @@ function( Lx, # Lexis object
         resp, # Events ('to' states)
      formula, # timescale ~ model
         xpos, # exposure states ('from' states)
+     verbose = TRUE,
           ... )
 {
 # Lexis object ?
